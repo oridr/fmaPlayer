ba0869a6a2f96789a41cf9f0e5deae71e90c5ba0
Ori Drori <oridr22@gmail.com>
2016-10-04 20:19:07 +0300

    [index] - injected all modules, and app works

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import 'styles/index.less';
 
 import angular from 'angular';
+import ngAnimate from 'angular-animate';
 
-import scApp from './components/scApp';
+import services from './services';
+import appMain from './components/appMain';
 
 angular.module('app', [
-	scApp
+	services,
+	ngAnimate,
+	appMain
 ]);
